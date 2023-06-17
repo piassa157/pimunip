eb4011a9291ce4fb7fa4f044cf396ad144995cee
Gabriel Piassa <39282662+piassa157@users.noreply.github.com>
2023-06-17 21:27:02 +0000

    Feat: added methods POST,PUT and DELETE

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -7,7 +7,7 @@ using System.Data;
 namespace pimUnipAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/person")]
     public class PeopleController : ControllerBase
     {
         private readonly string? _connectionString;
@@ -60,6 +60,122 @@ namespace pimUnipAPI.Controllers
             }
         }
     }
+
+    [HttpPost]
+    public IActionResult CreatePerson(Person person)
+    {
+        try
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "INSERT INTO Pessoa (NOME, CPF, ENDERECO_ID) VALUES (@Nome, @Cpf, @EnderecoId)";
+
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Nome", person.Nome);
+                    command.Parameters.AddWithValue("@Cpf", person.Cpf);
+                    command.Parameters.AddWithValue("@EnderecoId", person.EnderecoId);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return Ok("Person created successfully");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Error creating person: {ex.Message}");
+        }
+    }
+
+
+    [HttpPut("{id}")]
+    public IActionResult UpdatePerson(int id, Person person)
+    {
+        try
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "UPDATE Pessoa SET ";
+
+                if (!string.IsNullOrEmpty(person.Nome))
+                {
+                    query += "NOME = @Nome, ";
+                }
+
+                if (person.Cpf != null)
+                {
+                    query += "CPF = @Cpf, ";
+                }
+
+                if (person.EnderecoId != null)
+                {
+                    query += "ENDERECO_ID = @EnderecoId, ";
+                }
+
+                // Remove a vírgula extra no final da consulta
+                query = query.TrimEnd(',', ' ');
+
+                query += " WHERE ID = @Id";
+
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Nome", !string.IsNullOrEmpty(person.Nome) ? person.Nome : DBNull.Value);
+                    command.Parameters.AddWithValue("@Cpf", person.Cpf != null ? person.Cpf : DBNull.Value);
+                    command.Parameters.AddWithValue("@EnderecoId", person.EnderecoId != null ? person.EnderecoId : DBNull.Value);
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return Ok("Person updated successfully");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Error updating person: {ex.Message}");
+        }
+    }
+
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletePerson(int id)
+    {
+        if (id == 0)
+        {
+            return BadRequest("Invalid ID");
+        }
+
+        try
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "DELETE FROM Pessoa WHERE ID = @Id";
+
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return Ok("Person deleted successfully");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Error deleting person: {ex.Message}");
+        }
+    }
+    
+
+        
 
     public class Person
     {
